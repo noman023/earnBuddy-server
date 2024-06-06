9026c711582dff737b2ac7a6064652afe680b659
Mojammel Noman <molla.noman23@gmail.com>
2024-06-06 16:27:39 +0600

    added jwt api and sent jwt to client

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("hello");
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb atlas connection uri
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASS}@cluster0.faeme9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const db = client.db("earnBuddy");
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server Running on port ${port}`);
